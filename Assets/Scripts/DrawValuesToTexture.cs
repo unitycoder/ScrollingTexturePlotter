e9fa3672e982f65a3613c6fbd6119fd3e8b7da1c
mika <support@unitycoder.com>
2019-05-22 20:58:44 +0300

    init

---
+++ Assets/Scripts/DrawValuesToTexture.cs
@@ -0,0 +1,53 @@
+﻿// https://github.com/unitycoder/ScrollingTexturePlotter
+
+using UnityEngine;
+
+namespace UnityLibrary
+{
+    public class DrawValuesToTexture : MonoBehaviour
+    {
+        [Tooltip("Target material")]
+        public Material mat;
+
+        // our plotting texture
+        Texture2D tex;
+        Color32[] pixels;
+
+        int size = 1024;
+
+        // perlin noise values
+        float y;
+        float m1 = 0.07123f;
+        float m2 = 0.0156f;
+        float m3 = 0.0054f;
+
+        void Start()
+        {
+            // create texture with 1 pixel height
+            tex = new Texture2D(size, 1, TextureFormat.ARGB32, false, false);
+            tex.filterMode = FilterMode.Point;
+            tex.wrapMode = TextureWrapMode.Clamp;
+
+            // init pixel array
+            pixels = tex.GetPixels32(0);
+
+            // assign texture to our material
+            mat.SetTexture("_MainTex", tex);
+        }
+
+        void Update()
+        {
+            // fill pixels with some perlin noise values
+            for (int i = 0; i < size; i++)
+            {
+                float r = Mathf.PerlinNoise(i * m1, y * m1) - 0.5f;
+                float g = Mathf.PerlinNoise(i * m2, y * m2) - 0.5f;
+                float b = Mathf.PerlinNoise(i * m3, y * m3) - 0.5f;
+                pixels[i] = new Color(r, g, b, 1);
+            }
+            y += 1f;
+            tex.SetPixels32(pixels);
+            tex.Apply(false);
+        }
+    }
+}
---
+++ Assets/Shaders/RT-Update.shader
@@ -0,0 +1,49 @@
+﻿// https://github.com/unitycoder/ScrollingTexturePlotter
+
+Shader "UnityLibrary/Effects/ScrollingPlotter"
+{
+	Properties
+	{
+		_MainTex("Plotting Texture", 2D) = "white" {}
+	}
+
+	CGINCLUDE
+
+	#include "UnityCustomRenderTexture.cginc"
+
+	sampler2D _MainTex;
+
+	half4 frag(v2f_customrendertexture i) : SV_Target
+	{
+		//float xs = 1 / _CustomRenderTextureWidth;
+		float ys = 1 / _CustomRenderTextureHeight;
+		float2 uv = i.globalTexcoord;
+
+		// read pixel below current position
+		half3 c = tex2D(_SelfTexture2D, uv - float2(0, ys));
+
+		// read from plotting texture
+		half3 p = tex2D(_MainTex, float2(uv));
+
+		// for the bottom row, use our plotting texture
+		half b = (uv.y<ys);
+		c = lerp(c, p, b);
+
+		return half4(c,1);
+	}
+
+	ENDCG
+
+	SubShader
+	{
+		Cull Off ZWrite Off ZTest Always
+		Pass
+		{
+			Name "Update"
+			CGPROGRAM
+			#pragma vertex CustomRenderTextureVertexShader
+			#pragma fragment frag
+			ENDCG
+		}
+	}
+}
